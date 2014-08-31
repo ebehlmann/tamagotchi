60364d4b95548884a35ee3bec63461d99828078b
Emily Behlmann <ebehlmann@gmail.com>
2014-08-31 18:31:06 -0500

    adding initial tamagotchi files with first spec

---
+++ spec/specs.js
@@ -0,0 +1,12 @@
+describe('Tamagotchi', function() {
+	describe('initialize', function() {
+		it("sets the name and the starting properties", function() {
+			var myPet = Object.create(Tamagotchi);
+			myPet.initialize("my little pet");
+			myPet.name.should.equal("my little pet");
+			myPet.foodLevel.should.equal(10);
+			myPet.sleepLevel.should.equal(10);
+			myPet.activityLevel.should.equal(10);
+		});
+	});
+});
