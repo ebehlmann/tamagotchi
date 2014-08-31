93464f74a3003de9b2c533edb2d0d38702bb20bc
Emily Behlmann <ebehlmann@gmail.com>
2014-08-31 18:37:27 -0500

    set up Tomagotchi initialize function

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var Tamagotchi = {
+	initialize: function(petName, petType) {
+		this.name = petName;
+		this.type = petType;
+		this.foodLevel = 10;
+		this.sleepLevel = 10;
+		this.activityLevel = 10;
+	}
+}
--- spec/specs.js
+++ spec/specs.js
@@ -2,8 +2,9 @@ describe('Tamagotchi', function() {
 	describe('initialize', function() {
 		it("sets the name and the starting properties", function() {
 			var myPet = Object.create(Tamagotchi);
-			myPet.initialize("my little pet");
+			myPet.initialize("my little pet", "turtle");
 			myPet.name.should.equal("my little pet");
+			myPet.type.should.equal("turtle");
 			myPet.foodLevel.should.equal(10);
 			myPet.sleepLevel.should.equal(10);
 			myPet.activityLevel.should.equal(10);
